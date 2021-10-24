e7c95cefa5d79c71ccff724b7a76143e717edc01
Felix Sargent <felix@Felixs-MacBook-Pro.local>
2021-10-24 14:48:49 -0700

    Added tests and checking of security token

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+require('dotenv').config();
 
-const { TW_SANDBOX_API_TOKEN: TOKEN } = process.env;
+const TOKEN = process.env.TW_SANDBOX_API_TOKEN;
 
 const jsonParser = express.json();
 app
   .use(express.static(path.join(__dirname, 'static')))
   .get('/token', async (req, res) => {
+    // What is this endpoint for?
     const response = await fetch(
       'https://api.truework-sandbox.com/credentials/session',
       {
@@ -41,31 +43,33 @@ app
   });
 
 app.post('/webhook', jsonParser, async (req, res) => {
-  console.log(req.body);
   if (
-    req.body.hook.event === 'verification_request.state.change' &&
-    req.body.data.state === 'completed'
+    req.body.data?.credentials_session_token ??
+    undefined === process.env.SECURITY_TOKEN
   ) {
-    const verification_request_id = req.body.data.verification_request_id;
-    const response = await fetch(
-      `https://api.truework-sandbox.com/verification-requests/${verification_request_id}`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    console.log(await response.json());
-    res.send();
-  } else if (
-    req.body.hook.event === 'verification_request.state.change'
-  ) {
-    res.send();
-  } else {
-    res.status(500).send();
+    if (req.body.data.state === 'completed') {
+      // if the verification is completed, get the data from the api
+      const verification_request_id = req.body.data.verification_request_id;
+      const response = await fetch(
+        `https://api.truework-sandbox.com/verification-requests/${verification_request_id}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(
+        `VERIFICATION DATA FROM TRUEWORK - Not from webhook/n${await response.json()}`
+      );
+      res.send();
+    } else {
+      res.send();
+    }
   }
+  console.log(`Rejected webhook without valid request_id. Body: ${JSON.stringify(req.body)}`);
+  res.status(403).send();
 });
 
 module.exports = app;
--- test/server.spec.js
+++ test/server.spec.js
@@ -1,75 +1,42 @@
 const app = require('../server.js');
 const supertest = require('supertest');
 const requestWithSupertest = supertest(app);
+const fetch = require('node-fetch');
+
+// TODO: For tests, ensure env SECURITY_TOKEN=FnwaBrkdPFYwqycSRv_iBrqjmKeM_fxJWzNutNZi_Fg
 
 describe('Webhook Endpoint', () => {
-  const state_change_processing = {
-    hook: {
-      id: 68,
-      event: 'verification_request.state.change',
-      target: 'https://1345-135-180-37-121.ngrok.io/webhook',
-    },
-    data: {
-      verification_request_id:
-        'AAAAAAAAAmYAB3acTtZ3DS2-6EgllSFmL8O9VfY3QwtnvaG9jvw6XHOV',
-      state: 'processing',
-      credentials_session_token: 'FnwaBrkdPFYwqycSRv_iBrqjmKeM_fxJWzNutNZi_Fg',
-    },
-  };
-
   it('POST /webhook should show all users', async () => {
+    const state_change_processing = {
+      hook: {
+        id: 68,
+        event: 'verification_request.state.change',
+        target: 'https://1345-135-180-37-121.ngrok.io/webhook',
+      },
+      data: {
+        verification_request_id:
+          'AAAAAAAAAmYAB3acTtZ3DS2-6EgllSFmL8O9VfY3QwtnvaG9jvw6XHOV',
+        state: 'processing',
+        credentials_session_token:
+          'FnwaBrkdPFYwqycSRv_iBrqjmKeM_fxJWzNutNZi_Fg',
+      },
+    };
+
     const res = await requestWithSupertest
       .post('/webhook')
       .send(state_change_processing);
     expect(res.status).toEqual(200);
   });
+
+  it('POST /webhook should return 403 without a valid security token', async () => {
+    const res = await requestWithSupertest.post('/webhook').send('Hello');
+    expect(res.status).toEqual(403);
+  });
+
+	it('should have a GET /token endpoint', async () => {
+    const res = await requestWithSupertest.get('/token');
+    expect(res.status).toEqual(200);
+  });
+
 });
 
-// const state_change_completed = {
-//   hook: {
-//     id: 68,
-//     event: 'verification_request.state.change',
-//     target: 'https://1345-135-180-37-121.ngrok.io/webhook',
-//   },
-//   data: {
-//     verification_request_id:
-//       'AAAAAAAAAmYAB3acTtZ3DS2-6EgllSFmL8O9VfY3QwtnvaG9jvw6XHOV',
-//     state: 'completed',
-//     credentials_session_token: 'FnwaBrkdPFYwqycSRv_iBrqjmKeM_fxJWzNutNZi_Fg',
-//   },
-// };
-// const verification = {
-//   id: 'AAAAAAAAAmYAB3acTtZ3DS2-6EgllSFmL8O9VfY3QwtnvaG9jvw6XHOV',
-//   loan_id: null,
-//   state: 'completed',
-//   price: { currency: 'USD', amount: '34.95' },
-//   turnaround_time: {
-//     lower_bound: '20',
-//     upper_bound: '124',
-//     best_estimate: '48',
-//   },
-//   created: '2021-10-22T21:07:17.501388Z',
-//   date_of_completion: '2021-10-22T21:07:20.707917Z',
-//   target: {
-//     first_name: 'Eric',
-//     last_name: 'Bailey',
-//     contact_email: 'eric@truework.com',
-//     social_security_number: '***-**-1234',
-//     company: { id: 1, name: 'Truework - DEMO' },
-//     date_of_birth: '2020-02-02',
-//   },
-//   permissible_purpose: 'credit-application',
-//   type: 'employment',
-//   reports: [
-//     {
-//       id: 'AAAAAAAAAaEAC8_Uu4YwhVvbccnXFBVmE_ST2dQsxLq5wa4hSfH4cVAK',
-//       created: '2021-10-22T21:07:20.385360Z',
-//       current_as_of: '2021-10-22',
-//       verification_request: [Object],
-//       employer: [Object],
-//       employee: [Object],
-//       additional_notes: '',
-//     },
-//   ],
-//   use_case: 'preapproval',
-// };
